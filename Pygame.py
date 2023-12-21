a4fa19a0d9da2300b4a2ce53abf92319b0a30752
Kg2002tech <kg835455@gmail.com>
2023-12-21 22:21:27 +0300

    Project 001

---
+++ Pygame.py
@@ -0,0 +1,112 @@
+import random
+#from PIL import ImageTk,Image
+import tkinter as tk
+import os
+from pygame import mixer, mixer_music
+
+class MusicPlayer:
+     def __init__(self):
+          self.songs_list = []
+          self.alphabetical_order = {}
+          
+          self.app = tk.Tk()
+          self.app.geometry('300x300')
+          self.app.title('MY Music Player 1.0')
+          self.app.configure(bg='black')
+#BUTTONS  
+    
+          
+          play_button = tk.Button(self.app,text="Play",fg="Blue",font=('arial',20),command=self.playSong)
+          play_button.grid(row=1,column=0)
+          play_button.configure() 
+
+          pause_button = tk.Button(self.app,text='Pause',fg="Blue",font=('arial',20),command=self.pause_song)
+          pause_button.grid(row=1,column=1)
+          pause_button.configure()
+
+          skip_button = tk.Button(self.app,text="Skip",fg="Blue",font=('arial',20),command=self.skip_song)
+          skip_button.grid(row=1,column=2)
+          skip_button.configure()
+          
+          stop_button = tk.Button(self.app,text='Stop',fg="Blue",font=('arial',20),command=self.stopSong)
+          stop_button.grid(row=1,column=3)
+          stop_button.configure()
+
+          resume_button = tk.Button(self.app,text='Resume',fg="Blue",font=('arial',20),command=self.resume_song)
+          resume_button.grid(row=1,column=4)
+          resume_button.configure()
+
+          exit_button = tk.Button(self.app,text="Exit App ",fg="Blue",font=('arial',20),command=self.exit)
+          exit_button.grid(row=1,column=5)
+          exit_button.configure()
+
+     
+          #initialize the mixer
+          mixer.init()
+
+#Play the song
+     def playSong(self):
+        path = os.chdir('C:/Users/ADMIN/Music')
+        
+        self.songs_list = os.listdir(path)
+        for songs in self.songs_list:
+            song_id_list = [] 
+            self.alphabetical_order = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
+            for song_letter ,song_number in self.alphabetical_order.items():
+                 song_id_list.append(song_number) 
+                 first_song_check = songs[0] #shows the first starting letter or number of the song 
+               
+                 try: 
+                   if first_song_check.upper() == song_letter:
+                       self.current_song = 0
+                       mixer.music.set_volume(0.8)
+                       for num in range(0,26,song_number+1):
+                           self.current_song = 1+num
+                           mixer.music.load(self.songs_list[self.current_song])
+                           mixer.music.play()
+                       
+                 except Exception  as e:
+                    print(f'BUG : {e}')
+                    exit()
+                        
+  #Second command
+     def skip_song(self):
+        try:
+         if self.playSong:
+             
+            
+             #stop current song and move to next
+
+             self.current_song += 1
+             mixer.music.load(self.songs_list[self.current_song])
+             mixer_music.play()
+             if self.current_song > 1000:
+                 self.current_song = 0
+                 
+        
+        except Exception as e:
+                print(f"BUG2 :{e}. Please play song first before skipping ")
+               
+
+     def pause_song(self):  
+         mixer.music.pause()
+     
+     def stopSong(self):
+          mixer.music.stop()
+
+     def resume_song(self):
+          mixer.music.unpause()
+
+         
+
+
+     def exit(self):
+          self.app.destroy()
+
+     def runapp(self):
+          self.app.mainloop()
+
+
+my_app = MusicPlayer()
+my_app.runapp()
+
---
+++ musicplayer02.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+import os
+from turtle import width
+from pygame import  mixer
+my_app = tk.Tk()
+my_app.title("Music Player Alt Version 2")
+my_app ['bg'] = "black"
+
+
+
+
+#Initialize mixer
+mixer.init()
+mixer.music.set_volume(0.5)
+
+path = os.chdir('C:/Users/ADMIN/Music')
+songs_list = os.listdir(path)
+current_song_index = 0
+
+label2 = tk.Listbox(my_app,fg="Blue",font=('Helvetica',20),width=40)
+label2.grid(row=1,columnspan=1)
+
+#Display currently Playing music
+mess_label = tk.Label(my_app,fg="Blue",font=('Helvetica',20),width=40)
+mess_label.grid(row=2)
+
+
+def playSong():
+    
+        mixer.music.load(songs_list[current_song_index])
+        mixer.music.play()
+        mess_label.configure(text=f"Playing : {songs_list[current_song_index]}")
+        for songs in songs_list:
+              label2.insert(tk.END,songs)
+
+
+def skipSong():
+        if playSong:
+                 mixer.music.stop()
+                 global current_song_index
+                 current_song_index += 1  
+                 mixer.music.load(songs_list[current_song_index])
+                 mixer.music.play()
+                 mess_label.configure(text=f"Playing: {songs_list[current_song_index]}")
+                 
+    
+                     
+def  pauseSong():
+     mixer.music.pause()
+
+def  resumeSong():
+     mixer.music.unpause()   
+
+def exitApp():
+    my_app.destroy()
+
+
+#Butttons
+play_btn = tk.Button(my_app,text="PLAY ",fg="Blue",font=('arial',20),command=playSong,padx=7,pady=7)
+play_btn.grid(row=3,column=1)
+play_btn.configure()
+
+skip_btn = tk.Button(my_app,text="SKIP ",fg="Blue",font=('arial',20),command=skipSong,padx=6,pady=6)
+skip_btn.grid(row=3,column=2)
+skip_btn.configure()
+
+pause_btn = tk.Button(my_app,text="PAUSE ",fg="Blue",font=('arial',20),command=pauseSong,padx=7,pady=7)
+pause_btn.grid(row=3,column=3)
+pause_btn.configure()
+
+resume_btn = tk.Button(my_app,text="RESUME ",fg="Blue",font=('arial',20),command=resumeSong,padx=7,pady=7)
+resume_btn.grid(row=3,column=4) 
+resume_btn.configure()
+exit_btn = tk.Button(my_app,text="Exit ",fg="Blue",font=('arial',20),command=exitApp,padx=7,pady=7)
+exit_btn.grid(row=3,column=5)
+exit_btn.configure()
+def runApp():
+    my_app.mainloop()
+runApp()
